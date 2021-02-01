1a39b60cd941decd948b449a367e812e7e7dae53
Carsten Høyer <carsten@reload.dk>
2021-02-01 12:36:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NuxtApp } = require('@keystonejs/app-nuxt');
+
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+const PROJECT_NAME = 'Grayskull';
+const adapterConfig = { knexOptions: { connection: 'postgres://localhost/grayskull' } };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ name: PROJECT_NAME }),
+    new NuxtApp({
+      srcDir: 'src',
+      buildDir: 'dist',
+    }),
+  ],
+};
